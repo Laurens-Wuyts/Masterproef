7c9baa7a42d868966ccce9ac6756e286d6a19a58
laurens-wuyts <laurens.wuyts@gmail.com>
2020-04-04 20:56:24 +0200

    Import earlystopping

--- FastDepth.py
+++ FastDepth.py
@@ -7,6 +7,7 @@ import tensorflow as tf
 from tensorflow.keras.optimizers import SGD
 from tensorflow.keras.callbacks import TensorBoard
 from tensorflow.keras.callbacks import ModelCheckpoint
+from tensorflow.keras.callbacks import EarlyStopping
 
 from sklearn.metrics import classification_report
 from skimage import io
