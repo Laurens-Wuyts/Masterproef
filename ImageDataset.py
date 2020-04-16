b4dad219921aba1f3eb81ac20dfd3e7ede37bb26
laurens-wuyts <laurens.wuyts@gmail.com>
2020-04-16 21:22:48 +0200

    Changes to prediction

--- ImageDataset.py
+++ ImageDataset.py
@@ -19,6 +19,7 @@ def decode_image(img, ch):
 	return tf.image.convert_image_dtype(img, tf.float32)
 
 def process_path(path):
+	print(path)
 	depth_path = get_depth_path(path)
 	img = tf.io.read_file(path)
 	img = decode_image(img, 3)
