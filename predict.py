dd07ad3a16a9f0ffc36df22312b8e3e05ba83d6e
laurens-wuyts <laurens.wuyts@gmail.com>
2020-04-16 20:35:50 +0200

    Changes to prediction

--- predict.py
+++ predict.py
@@ -1,3 +1,5 @@
+import tensorflow as tf
+
 from tensorflow.keras.models import load_model
 from skimage import io
 from imutils import paths
@@ -6,7 +8,8 @@ import argparse
 import random
 import cv2
 import os
-import h5py
+
+from ImageDataset import Load_Dataset
 
 ap = argparse.ArgumentParser()
 ap.add_argument("-p", "--path",    required=True, help="path to root of project")
@@ -14,46 +17,51 @@ ap.add_argument("-m", "--model",   required=True, help="path to pre-trained netw
 ap.add_argument("-d", "--dataset", required=True, help="path to preprocessed dataset")
 args = vars(ap.parse_args())
 
-os.environ['TF_FORCE_GPU_ALLOW_GROWTH'] = 'true'
+DATA_PATH = args["path"] + "Data/"
+BS = 64
 
 print("[INFO] loading images...")
-images = []
-depths = []
+# images = []
+# depths = []
+# 
+# random.seed()
+# 
+# for _ in range(5):
+# 	idx = random.randint(0, f["testX"].shape[0])
+# 	i = f["testX"][idx]
+# 	images.append(i)
+# 	d = f["testY"][idx]
+# 	depths.append(d)
+# 
+# images = np.asarray(images)
+# depths = np.asarray(depths)
+# print(images.shape)
+test_ds  = Load_Dataset(DATA_PATH + args["dataset"] + "/preprocessed/Test/color", BS).unbatch().take(5)
 
-random.seed()
-
-with h5py.File(args["path"] + "Data/" + args["dataset"], 'r') as f:
-	for _ in range(5):
-		idx = random.randint(0, f["testX"].shape[0])
-		i = f["testX"][idx]
-		images.append(i)
-		d = f["testY"][idx]
-		depths.append(d)
-
-images = np.asarray(images)
-depths = np.asarray(depths)
-print(images.shape)
 
 print("[INFO] loading model...")
-model = load_model(args["path"] + args["model"])
+model = load_model(args["model"])
+
 print("[INFO] predicting...")
-	
-preds = model.predict(images)
+preds = model.predict(test_ds)
 
-preds = 255 * preds
-pred_imgs = preds.astype(np.uint8)
-print(pred_imgs.shape)
+#preds = 255 * preds
+#pred_imgs = preds.astype(np.uint8)
+#print(pred_imgs.shape)
 
-im_preds_color = np.stack((np.squeeze(pred_imgs),)*3, axis=-1)
-depths_color = np.stack((np.squeeze(depths),)*3, axis=-1)
-print(im_preds_color.shape, images.shape)
-image = None
+#im_preds_color = np.stack((np.squeeze(pred_imgs),)*3, axis=-1)
+#print(im_preds_color.shape, images.shape)
+#image = None
 
-for i in range(images.shape[0]):
-	temp = np.hstack((images[i], depths_color[i], im_preds_color[i]))
-	if image is None:
-		image = temp.copy()
-	else:
-		image = np.vstack((image, temp))
+for idx, (i, d) in enumerate(test_ds):
+	tmp_pred = tf.image.convert_image_dtype(preds[idx], tf.uint8)
+	tmp_pred = np.stack((np.squeeze(tmp_pred),)*3, axis=-1)
+	d = np.stack((np.squeeze(d),)*3, axis=-1)
+	print(idx, i.shape, d.shape, tmp_pred.shape())
+#	temp = np.hstack((i, d, im_preds_color[i]))
+#	if image is None:
+#		image = temp.copy()
+#	else:
+#		image = np.vstack((image, temp))
 
-cv2.imwrite(args["path"] + "Data/predictions.jpg", image)
+#cv2.imwrite(args["path"] + "Data/predictions.jpg", image)
